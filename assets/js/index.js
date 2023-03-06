7cac89a8b031948c599ac60182a6dd666d2090e8
IanAguilera24 <aguilera.marquez.ianalexis@gmail.com>
2023-03-06 01:11:44 -0600

    3ra version de ejercicios Javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
+//redireccionamiento del navbar
+var element = document.getElementById("Info-section");
+element.scrollIntoView();
+
+//cambio de logos por desplazamiento del navbar
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle('down',window.scrollY > 0);
@@ -13,6 +18,7 @@ window.addEventListener("scroll", function(){
     }
 });
 
+//funcionamiento del menu responsivo
 var menu = document.querySelector('.menu');
 var menuBtn = document.querySelector('.menu-btn');
 var closeBtn = document.querySelector('.close-btn');
@@ -24,3 +30,39 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     menu.classList.remove('active');
 })
+
+//Codigo para la galeria de ejercicios
+let galery = document.getElementById("galery");
+let seleccion = document.getElementById("seleccion");
+let iSeleccionada = document.getElementById("i");
+let description = document.getElementById("description");
+let information = document.getElementById("information");
+let lenguaje = document.getElementById("lenguaje");
+
+function cargar(item){
+    quitarBordes();
+    galery.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    iSeleccionada.src = item.getElementsByTagName("i")[0].src;
+    description.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    information.innerHTML = "Informacion del codigo ";
+
+    lenguaje.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+}
+
+function cerrar(){
+    galery.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
